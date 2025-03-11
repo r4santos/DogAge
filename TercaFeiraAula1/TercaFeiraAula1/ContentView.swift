3a244a0e78e998fba7cda025fdb32225e9767ad9
Rafael Santos <rafaelsantoslp11@gmail.com>
2025-03-11 11:53:17 -0300

    Add files via upload

---
+++ TercaFeiraAula1/TercaFeiraAula1/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  TercaFeiraAula1
+//
+//  Created by COTEMIG on 11/03/25.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    @State private var userAge: String = ""
+    @State private var dogAge: String = ""
+    @State private var showAlert = false
+    
+    func humanAgeCalculate() -> Void {
+        
+        if let age = Int(userAge), age > 0 {
+            let resultado = age * 7
+            dogAge = "A idade canina correspondente é: \(resultado) anos"
+        } else {
+            dogAge = "Digite um numero valido!"
+        }
+        
+        showAlert = true
+    }
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Calculadora de Idade Canina")
+            
+            TextField(
+                "Digite a idade humana",
+                text: $userAge
+            )
+            
+            Button(action: humanAgeCalculate){
+                Text("Calcular")
+            }
+            
+            .alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("Resultado"),
+                    message: Text(" \(dogAge)"),
+                    dismissButton: .default(Text("OK"))
+                )
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TercaFeiraAula1/TercaFeiraAula1/TercaFeiraAula1App.swift
@@ -0,0 +1,17 @@
+//
+//  TercaFeiraAula1App.swift
+//  TercaFeiraAula1
+//
+//  Created by COTEMIG on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TercaFeiraAula1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
